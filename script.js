529b491958a6e38f67541ed67dcddc6d119bad32
Nicolasudry <110941305+Nicolasudry@users.noreply.github.com>
2022-11-06 23:45:26 -0300

    Empece con funciones y termine de subir las fotos
    Subi las fotos de los productos e hice el carrito funcional

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("hola mundo");
-const Carrito=[]
+
+//let Total=0;
+//function palcarrito(precio) {
+function palcarrito(tallemin, tallemax, talleingresado, precio, zapato) {
+    if (talleingresado > tallemin || talleingresado < tallemax ) {
+        
+    
+     
+        Total += parseInt (precio);
+        document.getElementById("Total").innerHTML = "TOTAL: " + "$" + Total ;
+        document.getElementById("Items").innerHTML += (zapato + ' talle ' + talleingresado + ',');
+        //talleingresado = 0;
+    }
+    else{alert("ingrese un talle valido")}
+    return;
+}
+
+
+function Borrar() {
+    Total = 0;
+    document.getElementById("Total").innerHTML = "TOTAL:" + " $" + 0 ;}
+
+
+
