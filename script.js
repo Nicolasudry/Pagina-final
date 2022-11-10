f0900b61733c8caba87d1420500dc71c38a762f0
Nicolasudry <110941305+Nicolasudry@users.noreply.github.com>
2022-11-10 13:23:31 -0300

    Arregle lo de conseguir los botones con el query

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 
 //let Total=0;
 //function palcarrito(precio) {
-    function mostrarCantidad (a){
-        let cantidad = document.getElementById(a).value
+    function mostrarCantidad (){
+        let cantidad = document.getElementById("tallem1").value
         console.log(cantidad)
     }
-    document.getElementById("botonm2").addEventListener("click",mostrarCantidad(tallem2))
-    function mostrarCantidad (){
+    mostrarCantidad
+   // document.getElementById("botonm2").addEventListener("click", ()=>mostrarCantidad(tallem2))
+    
        
-    let Total=0
-function palcarrito( precio, zapato) {
+     let Total=0
+     
+function palcarrito(precio,zapato) {
          //document.getElementById("talleingresado").innerHTML
     
         
     
         
-        Total += parseInt (precio);
+        Total+= precio ;
+        console.log(Total)
         document.getElementById("Total").innerHTML = "TOTAL: " + "$" + Total ;
-        document.getElementById("Items").innerHTML += (zapato + ' talle ' + cantidad + ',');
+        document.getElementById("Items").innerHTML += (zapato + ' talle '  + ',' );
         //talleingresado = 0;
+        
     
     
     return;}
-    document.getElementById("")
+
 
 
 
 function Borrar() {
     Total = 0;
-    document.getElementById("Total").innerHTML = "TOTAL:" + " $" + 0 ;
-    document.getElementById("Items").innerHTML= "";}
-
+    document.getElementById("Total").innerHTML = "Total:" + " $" + 0 ;
+    document.getElementById("Items").innerHTML= "Items:";}
+//let Elegidom1 = document.getElementById("tallem1").value
+//console.log(Elegidom2)
 document.querySelector("#vaciador").addEventListener("click",Borrar)
-document.querySelector("#botonm2").addEventListener("click",palcarrito(12500,'zapato negro mujer'))
-document.querySelector("#botonm1").addEventListener("click",palcarrito(11500,'zapato marron mujer'))
-document.querySelector("#botonh1").addEventListener("click",palcarrito(14500,'zapato negro hombre'))
-document.querySelector("#botonh2").addEventListener("click",palcarrito(12500,'zapato marron hombre'))
 
-document.querySelector("#botonm2").addEventListener("click",gettalle("tallem2"))
-function gettalle(a){
-        let talleseleccionado=document.getElementById(a).innerHTML;
-        console.log(talleseleccionado);}
+function saludar(){
+    console.log('hola')
+};
+document.querySelector("#botonm1").addEventListener("click",function(){palcarrito(11500,'zapato marron mujer')})
+document.querySelector("#botonh1").addEventListener("click",function () {palcarrito(14500,'zapato negro hombre')})
+document.querySelector("#botonh2").addEventListener("click",function(){palcarrito(13500,'zapato marron hombre')})
+document.querySelector("#botonm2").addEventListener("click",function() {palcarrito(12500,'Zapato negro mujer')})
+
