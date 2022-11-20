ac1e7efe3341b9073ace2add2d1a5b7870e4cacd
Nicolasudry <110941305+Nicolasudry@users.noreply.github.com>
2022-11-20 12:15:38 -0300

    Termine con el carrito y cambie las indentaciones para dejar la lectura mas comoda.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 
-//let Total=0;
+    
 //function palcarrito(precio) {
-    function mostrarCantidad (){
-        let cantidad = document.getElementById("tallem1").value
-        console.log(cantidad)
-    }
-    mostrarCantidad
-   // document.getElementById("botonm2").addEventListener("click", ()=>mostrarCantidad(tallem2))
+    
+   
     
        
      let Total=0
      
-function palcarrito(precio,zapato) {
-         //document.getElementById("talleingresado").innerHTML
+function palcarrito(precio,zapato,talle) {
+         
     
         
     
@@ -20,8 +16,8 @@ function palcarrito(precio,zapato) {
         Total+= precio ;
         console.log(Total)
         document.getElementById("Total").innerHTML = "TOTAL: " + "$" + Total ;
-        document.getElementById("Items").innerHTML += (zapato + ' talle '  + ',' );
-        //talleingresado = 0;
+        document.getElementById("Items").innerHTML += (zapato + ' talle '  + talle + ',' );
+        
         
     
     
@@ -34,19 +30,15 @@ function Borrar() {
     Total = 0;
     document.getElementById("Total").innerHTML = "Total:" + " $" + 0 ;
     document.getElementById("Items").innerHTML= "Items:";}
-//let Elegidom1 = document.getElementById("tallem1").value
-//console.log(Elegidom2)
+
 document.querySelector("#vaciador").addEventListener("click",Borrar)
 
 function saludar(){
     console.log('hola')
 };
-document.querySelector("#botonm1").addEventListener("click",function(){palcarrito(11500,'zapato marron mujer')})
-document.querySelector("#botonh1").addEventListener("click",function () {palcarrito(14500,'zapato negro hombre')})
-document.querySelector("#botonh2").addEventListener("click",function(){palcarrito(13500,'zapato marron hombre')})
-document.querySelector("#botonm2").addEventListener("click",function() {palcarrito(12500,'Zapato negro mujer')})
+document.querySelector("#botonm1").addEventListener("click",function(){palcarrito(11500,'zapato marron mujer',document.getElementById("tallem1").value)})
+document.querySelector("#botonh1").addEventListener("click",function () {palcarrito(14500,'zapato negro hombre',document.getElementById("talleh1").value)})
+document.querySelector("#botonh2").addEventListener("click",function(){palcarrito(13500,'zapato marron hombre',document.getElementById("talleh2").value)})
+document.querySelector("#botonm2").addEventListener("click",function() {palcarrito(12500,'Zapato negro mujer',document.getElementById("tallem2").value)})
 
-let vtallem2= document.getElementById("#tallem2")
-let vtallem1= document.getElementById("#tallem1")
-let vtalleh1= document.getElementById("#talleh1")
-let vtalleh2= document.getElementById("#talleh2")
+
